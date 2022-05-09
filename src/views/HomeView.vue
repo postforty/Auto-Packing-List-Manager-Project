32c27e24eb63a57d2e97df15e3ba35fa68334db6
postforty <75767073+postforty@users.noreply.github.com>
2022-05-09 16:11:34 +0900

    [UPDATE] auto_packing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,38 +2,43 @@
   <div>
     <h1>Packing List 자동 출력</h1>
     <div>
-      <label for="">로트번호</label>
+      <label for="">로트번호 입력</label>
       <input v-model="lotNo" type="text" name="" id="" />
     </div>
     <div>
-      <label for="">고객선택</label>
+      <label for="">고객사 선택</label>
       <select v-model="selectedCustomer" name="" id="">
         <option :key="i" v-for="(customer, i) in customers">
           {{ customer }}
         </option>
       </select>
-      <button
-        class="btn btn-primary me-1"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-      >
-        신규 고객 등록
+      <button data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        신규 고객사 추가
       </button>
     </div>
     <div>
       <button @click="doCreate">Packing List 생성</button>
     </div>
+    <!-- 신규 고객사 등록 모달 -->
     <div>
       <slot-modal modalId="staticBackdrop">
-        <template v-slot:title>신규 고객 등록</template>
+        <template v-slot:title>신규 고객사 등록</template>
         <template v-slot:body>
           <p>
-            <label for="">고객사명</label
-            ><input v-model="newCustomer" type="text" name="" id="" />
+            <label for="">고객사 명</label
+            ><input
+              v-model="newCustomer"
+              type="text"
+              name=""
+              ref="newCustomerModal"
+            />
           </p>
         </template>
         <template v-slot:footer>
-          <button @click="addCustomer">저장</button>
+          <button class="btn btn-secondary" data-bs-dismiss="modal">
+            닫기
+          </button>
+          <button class="btn btn-primary" @click="addCustomer">저장</button>
         </template>
       </slot-modal>
     </div>
@@ -52,8 +57,12 @@ export default {
     }
   },
   setup() {},
-  created() {},
-  mounted() {},
+  created() {
+    this.customers.unshift('고객사를 선택하세요')
+  },
+  mounted() {
+    this.selectedCustomer = this.customers[0]
+  },
   unmounted() {},
   methods: {
     doCreate() {
@@ -61,6 +70,7 @@ export default {
     },
     addCustomer() {
       this.customers.push(this.newCustomer)
+      this.$refs.newCustomerModal.value = ''
     }
   }
 }
