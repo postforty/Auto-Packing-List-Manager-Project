6d750226f4df7db6a6d8bf8fdae25eeb74657907
postforty <75767073+postforty@users.noreply.github.com>
2022-06-03 17:16:35 +0900

    [UPDATE] CSS 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,22 +21,9 @@
       />
     </div>
     <div id="top" class="py-5 text-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        fill="currentColor"
-        class="bi bi-file-earmark-text mb-4"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"
-        />
-        <path
-          d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"
-        />
-      </svg>
-      <h2>Packing List Manager</h2>
+      <a href="http://localhost:8080/">
+        <h2><em>Packing List Manager</em></h2>
+      </a>
     </div>
     <div class="p-5">
       <div class="form-floating">
@@ -125,7 +112,24 @@
           data-bs-target="#staticBackdrop"
           class="btn btn-outline-primary"
         >
-          Packing List
+          <span style="position: relative; top: 25%">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-file-earmark-text mb-4"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"
+              />
+              <path
+                d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"
+              />
+            </svg>
+          </span>
+          <span style="position: relative; top: 5%">Packing List</span>
         </button>
       </div>
     </div>
