749d751a16a28baeadf11b4a78731f906102bcfb
postforty <ubithus@gmail.com>
2022-06-08 08:46:52 +0900

    [DELETE] test 라우터 삭제

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/test',
-    name: 'test',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "test" */ '../views/TestView.vue')
   }
+  // {
+  //   path: '/test',
+  //   name: 'test',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "test" */ '../views/TestView.vue')
+  // }
 ]
 
 const router = createRouter({
